80a09e706a6b7e8b80038031cde10c3a89a14bc4
Ana Carolina <garcone.anaxx@gmail.com>
2021-09-26 21:35:43 -0300

    adicionando e renderizando de forma simples os filmes

---
+++ script.js
@@ -0,0 +1,74 @@
+// let array = [{
+//   nome: 'form.nome',
+//   imagem: 'fomr.imagem',
+//   genero: 'form.genero',
+//   nota: 'form.nota',
+//   id: 123123123123123456,
+//   checked: false
+// }];
+
+const movieItens = [];
+
+const addMovie = (nome, imagem, genero, nota) => {
+  const movie = {
+    nome: nome,
+    imagem: imagem,
+    genero: genero,
+    nota: nota,
+    id: Date.now(),
+    checked: false
+  }
+
+  movieItens.push(movie);
+  renderMovie(movie);
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+
+  const inputNome = document.querySelector('#nome');
+  const inputGenero = document.querySelector('#genero');
+  const inputImagem = document.querySelector('#imagem');
+  const inputNota = document.querySelector('#nota');
+
+  const nome = inputNome.value;
+  const genero = inputGenero.value;
+  const imagem = inputImagem.value;
+  const nota = inputNota.value;
+
+  if (nome !== '' && genero !== '' && imagem !== '' && nota !== '') {
+    addMovie(nome, imagem, genero, nota);
+  }
+});
+
+const renderMovie = (movie) => {
+  const list = document.querySelector('.js-movie-list');
+
+  const listItens = document.createElement('div');
+  listItens.setAttribute('class', 'list-itens');
+
+  const listNome = document.createElement('h2');
+  listNome.setAttribute('class', 'list-nome');
+
+  const listImg = document.createElement('img');
+  listImg.setAttribute('src', movie.imagem);
+
+  const listGenero = document.createElement('span');
+  listGenero.setAttribute('class', 'list-span');
+
+  const listNota = document.createElement('span');
+  listNota.setAttribute('class', 'list-span-nota');
+
+
+  listNome.innerHTML = movie.nome;
+  listGenero.innerHTML = movie.genero;
+  listNota.innerHTML = movie.nota;
+
+  listItens.append(listNome);
+  listItens.append(listImg);
+  listItens.append(listGenero);
+  listItens.append(listNota);
+  list.append(listItens);
+
+}
