63fabd51b0b1d0dc327a314188cb4195685213fa
Ana Carolina <garcone.anaxx@gmail.com>
2021-10-03 17:38:08 -0300

    crud getmovies e delete

--- frontend/script.js
+++ frontend/script.js
@@ -10,9 +10,36 @@
 const url = 'http://localhost:3000/movies';
 const movieItens = [];
 
+const getMovies = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data)
+
+  const list = document.querySelector('.js-movie-list');
+
+  data.map((movie) => {
+    list.insertAdjacentHTML('beforeend', `
+    <div class="list-itens" data-key=${movie.id}>
+      <input id=${movie.id} type="checkbox"/>
+      <label for=${movie.id} class="check"></label>
+      <button onclick="deleteMovie(${movie.id})" class="delete-icon"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+      <h2>${movie.nome}</h2>
+      <img src=${movie.imagem}>
+      <div class="genero-nota">
+        <p>${movie.genero}</p>
+        <span>${movie.nota}</span>
+      </div>
+    </div>
+  `)
+  }) 
+}
+
+getMovies();
+
 const form = async (evento) => {
   evento.preventDefault();
 
+  const list = document.querySelector('.js-movie-list');
   const inputNome = document.querySelector('#nome');
   const inputGenero = document.querySelector('#genero');
   const inputImagem = document.querySelector('#imagem');
@@ -37,36 +64,25 @@ const form = async (evento) => {
   const result = await response.json();
   if(result) {
     getMovies();
-    inputNome.value = '';
-    inputGenero.value = '';
-    inputImagem.value = '';
-    inputNota.value = '';
-    inputNome.focus();
   }
+
+  inputNome.value = '';
+  inputGenero.value = '';
+  inputImagem.value = '';
+  inputNota.value = '';
+  list.innerHTML = '';
+  inputNome.focus();
   
 }
 
-const getMovies = async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data)
-
+const deleteMovie = async (id) => {
   const list = document.querySelector('.js-movie-list');
+  const req = new Request(`${url}/${id}`, {
+    method: 'DELETE',
+  })
+  const res = await fetch(req);
+  const data = await res.json();
 
-  data.map((movie) => {
-    list.insertAdjacentHTML('beforeend', `
-    <div class="list-itens" data-key=${movie.id}>
-      <input id=${movie.id} type="checkbox"/>
-      <label for=${movie.id} class="check"></label>
-      <button class="delete-icon"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
-      <h2>${movie.nome}</h2>
-      <img src=${movie.imagem}>
-      <div class="genero-nota">
-        <p>${movie.genero}</p>
-        <span>${movie.nota}</span>
-      </div>
-    </div>
-  `)
-  }) 
+  list.innerHTML = '';
+  getMovies();
 }
-getMovies();
