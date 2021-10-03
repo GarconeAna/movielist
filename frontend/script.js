82432548b6273b3b8488461e0ee30eeaa8e567e6
Ana Carolina <garcone.anaxx@gmail.com>
2021-10-03 18:18:50 -0300

    crud - putmovie

--- frontend/script.js
+++ frontend/script.js
@@ -9,6 +9,14 @@
 
 const url = 'http://localhost:3000/movies';
 const movieItens = [];
+let edit = false;
+let idEdit = 0;
+
+const list = document.querySelector('.js-movie-list');
+const inputNome = document.querySelector('#nome');
+const inputGenero = document.querySelector('#genero');
+const inputImagem = document.querySelector('#imagem');
+const inputNota = document.querySelector('#nota');
 
 const getMovies = async () => {
   const response = await fetch(url);
@@ -23,6 +31,7 @@ const getMovies = async () => {
       <input id=${movie.id} type="checkbox"/>
       <label for=${movie.id} class="check"></label>
       <button onclick="deleteMovie(${movie.id})" class="delete-icon"><i class="fa fa-trash-o" aria-hidden="true"></i></button>
+      <button onclick="putMovie(${movie.id})" class="delete-icon"><i class="fa fa-pencil" aria-hidden="true"></i></button>
       <h2>${movie.nome}</h2>
       <img src=${movie.imagem}>
       <div class="genero-nota">
@@ -39,11 +48,11 @@ getMovies();
 const form = async (evento) => {
   evento.preventDefault();
 
-  const list = document.querySelector('.js-movie-list');
-  const inputNome = document.querySelector('#nome');
-  const inputGenero = document.querySelector('#genero');
-  const inputImagem = document.querySelector('#imagem');
-  const inputNota = document.querySelector('#nota');
+  // const list = document.querySelector('.js-movie-list');
+  // const inputNome = document.querySelector('#nome');
+  // const inputGenero = document.querySelector('#genero');
+  // const inputImagem = document.querySelector('#imagem');
+  // const inputNota = document.querySelector('#nota');
 
   const movie = {
     nome: inputNome.value,
@@ -52,18 +61,34 @@ const form = async (evento) => {
     nota: inputNota.value,  
   }
 
-  const request = new Request(`${url}/add`, {
-    method: 'POST',
-    body: JSON.stringify(movie),
-    headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  });
-
-  const response = await fetch(request);
-  const result = await response.json();
-  if(result) {
-    getMovies();
+  if(!edit) {
+    const req = new Request(`${url}/add`, {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
+  
+    const res = await fetch(req);
+    const result = await res.json();
+    if(result) {
+      getMovies();
+    }    
+  } else {
+    const req = new Request(`${url}/${idEdit}`, {
+      method: 'PUT',
+      body: JSON.stringify(movie),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    });
+  
+    const res = await fetch(req);
+    const result = await res.json();
+    if(result) {
+      getMovies();
+    }    
   }
 
   inputNome.value = '';
@@ -73,6 +98,20 @@ const form = async (evento) => {
   list.innerHTML = '';
   inputNome.focus();
   
+}
+
+const putMovie = async (id) => {
+  edit = true;
+  idEdit = id;
+  const res = await fetch(`${url}/${id}`);
+  const movie = await res.json(); 
+  console.log(movie.nome);
+
+  inputNome.value = movie.nome;
+  inputGenero.value = movie.genero;
+  inputImagem.value = movie.imagem;
+  inputNota.value = movie.nota;
+
 }
 
 const deleteMovie = async (id) => {
